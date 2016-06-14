f7960940a18f66a9bf1d54a422fd030b874261ac
Martin Larsson <martin@mlarsson.se>
2016-06-14 18:29:39 +0200

    square digits

---
+++ src/se/mlarsson/SquareDigits.java
@@ -0,0 +1,26 @@
+package se.mlarsson;
+
+/**
+ * Class for squaring individual digits in a number.
+ */
+public class SquareDigits {
+
+    /**
+     * Returns a number with individual squares of each digit in input number.
+     * @param value input value
+     * @return squared digits
+     * @throws IllegalArgumentException
+     */
+    public int square(int value) throws IllegalArgumentException {
+        if (value < 0) throw new IllegalArgumentException("" +
+                "Value must be positive.");
+        int squared = 0;
+        int length = 0;
+        while (value > 0) {
+            squared += (Math.pow(value%10,2) * Math.pow(10,length));
+            value = value/10;
+            length = (int)Math.log10(squared)+1;
+        }
+        return squared;
+    }
+}
---
+++ test/se/mlarsson/SquareDigitsTest.java
@@ -0,0 +1,48 @@
+package se.mlarsson;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class SquareDigitsTest {
+    private SquareDigits sd;
+
+    @Before
+    public void setUp() {
+        sd = new SquareDigits();
+    }
+
+    @Test
+    public void testsSquareZero() throws Exception {
+        int expected = 0;
+        int actual = sd.square(0);
+        assertEquals("Testing digit square of 0", expected, actual);
+    }
+
+    @Test
+    public void testsSquareOne() throws Exception {
+        int expected = 1;
+        int actual = sd.square(1);
+        assertEquals("Testing digit square of 1", expected, actual);
+    }
+
+    @Test
+    public void testsSquareTwo() throws Exception {
+        int expected = 4;
+        int actual = sd.square(2);
+        assertEquals("Testing digit square of 2", expected, actual);
+    }
+
+    @Test
+    public void testsSquareTwelve() throws Exception {
+        int expected = 14;
+        int actual = sd.square(12);
+        assertEquals("Testing digit square of 12", expected, actual);
+    }
+
+    @Test
+    public void testsSquare359() throws Exception {
+        int expected = 92581;
+        int actual = sd.square(359);
+        assertEquals("Testing digit square of 359", expected, actual);
+    }
+}
